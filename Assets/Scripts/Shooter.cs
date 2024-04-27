833f0e233964d4df247c23ac777d8f92d58fcdfa
Amoong <moo1323@naver.com>
2024-04-27 22:10:56 +0900

    Add enemy shooting

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float firingRate = 0.2f;
+    [SerializeField] float baseFiringRate = 0.2f;
 
-    public bool isFiring;
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = 0.1f;
+
+    [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
 
     void Start()
     {
-
+        if (useAI)
+        {
+            isFiring = true;
+        }
     }
 
     void Update()
@@ -41,13 +50,22 @@ public class Shooter : MonoBehaviour
         while (true)
         {
             GameObject instance = Instantiate(projectilePrefab, gameObject.transform.position, Quaternion.identity);
+
             Rigidbody2D rb = instance.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
                 rb.velocity = transform.up * projectileSpeed;
             }
+
             Destroy(instance, projectileLifetime);
-            yield return new WaitForSeconds(firingRate);
+
+            yield return new WaitForSeconds(GetRandomFiringRate());
         }
+    }
+
+    float GetRandomFiringRate()
+    {
+        float rate = Random.Range(baseFiringRate - firingRateVariance, baseFiringRate + firingRateVariance);
+        return Mathf.Clamp(rate, minimumFiringRate, float.MaxValue);
     }
 }
